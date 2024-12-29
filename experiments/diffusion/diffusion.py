0eacf239fed998e70c1f3db72c4fcd5fb5f42e1b
Luca Muscarnera <luca@DESKTOP-60SPNQS.>
2024-12-29 19:25:07 +0100

    skeleton diffusion experiment

--- experiments/diffusion/diffusion.py
+++ experiments/diffusion/diffusion.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
 
-import libsfinder
+# imports
 
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import numpy as np
+
+# custom imports
+
+import libsfinder
 from jaxneuralnetworks import network as jnn
 
-print("hello world!")
+
+def main():
+  # simulation data
+
+  N     = 100
+  steps = 10
+
+  # define the initial data and the final data
+  X_0 = np.random.uniform(N, 2)
+  X_f = np.random.randn(N,2)
+
+  # initialize buffer
+  B = X_0.copy()
+  for i in range(steps):
+    X_next = diffusionengine.homotopy(X_0, X_f, i)
+    W      = np.linalg.pinv(B) @ X_next
+
+if __name__ == '__main__':
+  main()
